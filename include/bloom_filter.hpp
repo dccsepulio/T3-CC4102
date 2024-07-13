ae01bd95c068d9085521cb9a551f577c475ec645
Marcelo <marcelo.chuang@ug.uchile.cl>
2024-07-12 21:09:44 -0400

    murmurhash and general bloom filter

---
+++ include/bloom_filter.hpp
@@ -0,0 +1,24 @@
+#ifndef BLOOM_FILTER_H
+#define BLOOM_FILTER_H
+
+#include <bitset>
+#include <vector>
+#include <string>
+
+const int FILTER_SIZE = 1000;  // Tamaño del arreglo M en bits
+const int HASH_FUNCTIONS = 3;  // Número de funciones hash
+
+class BloomFilter {
+private:
+    std::bitset<FILTER_SIZE> bitArray;  // Arreglo M de bits
+    std::vector<uint32_t> seeds;  // Semillas para las funciones hash
+
+public:
+    BloomFilter();
+    void insert(const std::string &element);
+    bool contains(const std::string &element) const;
+};
+
+void readDataFromCSV(const std::string &filename, BloomFilter &bloomFilter);
+
+#endif // BLOOM_FILTER_H
